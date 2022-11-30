48613bf0bd0ae68f1b9f926e2b9c7963cfa7e7a6
techsayright <darshan2001.dp@gmail.com>
2022-11-30 15:52:05 +0530

    updates

--- ksql.sh
+++ ksql.sh
@@ -6,13 +6,15 @@ docker-compose exec -T ksqldb-cli ksql http://ksqldb-server:8088 <<-EOF
     set 'auto.offset.reset'='earliest';
 
 
-    CREATE STREAM courses WITH (KAFKA_TOPIC='demo.class.courses', VALUE_FORMAT='AVRO');
+    CREATE STREAM courses_src WITH (KAFKA_TOPIC='demo.class.courses', VALUE_FORMAT='AVRO');
+
+    CREATE STREAM courses1 WITH (KAFKA_TOPIC='demo.class.courses', VALUE_FORMAT='AVRO');
 
     CREATE OR REPLACE STREAM class_boost WITH (
     kafka_topic = 'demo.class.class_boost',
     VALUE_FORMAT='AVRO'
 )   AS
-    SELECT *, rowtime AS time FROM courses
+    SELECT *, rowtime AS time FROM courses1
     EMIT CHANGES;
 
 
